043f11583fbb44d6467461404123391b37652bc3
JuusoAsd <jahlroos96@gmail.com>
2021-10-01 11:07:55 +0300

    added comments to reading data

--- src/get_data.py
+++ src/get_data.py
@@ -11,41 +11,46 @@ from pprint import pprint
 
 
 conf = dotenv_values(".env")
+
+# initialize the web3 object using infura endpoint
+# endpoint can also be for example your own ethereum node
 web3 = Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{conf['infura']}"))
 
+# saving abi's in dictionary for addresses so they do not have to be re-read
 abi_implementation = {'address':'abi'}
 
+# replicating rari interest rate calculation - https://docs.rari.capital/fuse/#calculating-the-apy-using-rate-per-block
 def annualized_return(rate):
     per_day = 6500
     days = 365
     factor = 1*10**18
     return (((rate/factor*per_day + 1) ** days) - 1 ) * 100
-
-def main():
-    pool_address = '0x989273ec41274C4227bCB878C2c26fdd3afbE70d'
-    folder = os.path.join(os.getcwd(), "abi","rari_abi.json")
-    with open(folder) as f:
-        abi = json.load(f)
-
-    contract = web3.eth.contract(
-            address=web3.toChecksumAddress(pool_address), abi=abi
-        )
-
-    lend = contract.functions.supplyRatePerBlock().call()
-    borrow = contract.functions.borrowRatePerBlock().call()
-
-    print(annualized_return(lend), annualized_return(borrow))
-
-   
     
 def get_rari_fuse():
+    # https://docs.rari.capital/contracts/#rari-governance FusePoolDirectory
     comptroller_address='0x835482FE0532f169024d5E9410199369aAD5C77E'
+    
+    # Some contracts have "proxy implementation", where implementation and contract are in different address
+    # The "implementation address" can be usually found by querying the address
+    # In this case, not for some reason
+    # as workaround, going to etherscan for the address: https://etherscan.io/address/0x835482FE0532f169024d5E9410199369aAD5C77E
+    # going to contract > read as proxy
+    # https://etherscan.io/address/0x835482FE0532f169024d5E9410199369aAD5C77E#readProxyContract
+    # there you can find "abi for the implementation contract" -address
     comptroller_implementation='0xbC81C8fBB73B5825bA6CC7C4dE1fE92004Cc80c6'
+    
+    # gets the comptroller contract object
     comptroller = get_abi_implementation_separate(
         comptroller_address,
         comptroller_implementation
         )
+
+    # integers for the largest pools
     important_pools = [6,18,8,9,7,24]
+    
+    # contracts have functions which are defined in ABI
+    # all functions can be checked by, for example, print(*contract.functions, sep='\n')
+    # this function gets all pools from comptroller
     pool_list = comptroller.functions.getAllPools().call()
     for nr in important_pools:
         print()
@@ -56,27 +61,34 @@ def get_fuse_pool(pool_info):
     print(pool_info[0])
     pool_address = pool_info[2]
     pool_contract = get_pool_contract(pool_address)
-    #print(*pool_contract.functions, '\n')
+    
+    # pool contact has multiple markets like ETH, DAI, USDC...
+    # looping through these "markets" and getting the interest rates for those
     all_markets = pool_contract.functions.getAllMarkets().call()
     for market in all_markets:
         market_contract = get_market_contract(market)
-        #get_rari_rate(market_contract)
-        print(market_contract.functions.interestRateModel().call())
+        get_rari_rate(market_contract)
         
 def get_rari_rate(contract):
+    # symbol for the token (ETH, DAI, USDC)
     symbol = contract.functions.symbol().call()
+    
+    # information required for computing the interest rates
     lend = contract.functions.supplyRatePerBlock().call()
     borrow = contract.functions.borrowRatePerBlock().call()
-
     print(symbol, round(annualized_return(lend), 2), round(annualized_return(borrow),2))
 
-
+# create contract object for the pool
 def get_pool_contract(address):
+    # create the contract
     initial_contract = web3.eth.contract(
             address=address,
             abi=get_abi(address)
         )
+    
+    # contract has comptrollerImplementation function which returns the address for the pool implementation
     implementation_address = initial_contract.functions.comptrollerImplementation().call()
+    # returns the pool contract
     return web3.eth.contract(
             address=address,
             abi=get_abi(implementation_address)
@@ -93,13 +105,15 @@ def get_market_contract(address):
             abi=get_abi(implementation_address)
         )
 
+# creating the contract-object when contract and implementation are in different addresses
 def get_abi_implementation_separate(address, implementation):
 
     return web3.eth.contract(
             address=address,
             abi=get_abi(implementation)
         )
-
+        
+# etherscan has api which allows to read abis for (verified) smart contracts
 def get_abi(address):
     if address in abi_implementation:
         return abi_implementation[address]
